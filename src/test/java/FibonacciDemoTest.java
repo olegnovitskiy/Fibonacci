cfe455896aafb196bf957b02a81f698a3aa135da
Oleg Novitskiy <oleg.novitskiy.dev@gmail.com>
2016-04-28 11:16:56 +0300

    Done!

--- src/test/java/FibonacciDemoTest.java
+++ src/test/java/FibonacciDemoTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.assertEquals;
 public class FibonacciDemoTest {
     @Test
     public void testFib() throws Exception {
-        int cases[][] = {{0, 0}, {1,1}, {2, 1}, {3, 2}};
+        int cases[][] = {{0, 0}, {1,1}, {2, 1}, {3, 2}, {4, 3}, {5, 5}};
         for (int[] aCase : cases) {
             assertEquals(aCase[1], fib(aCase[0]));
         }
@@ -15,7 +15,7 @@ public class FibonacciDemoTest {
         if (number == 0) {
             return 0;
         }
-        if (number <= 2) {
+        if (number == 1) {
             return 1;
         }
         return fib(number - 1) + fib(number - 2);
